9e8f35f1e35ced8431c2a945202af22f1eb23c20
Matthew Stratford <hi@mstratford.net>
2018-10-20 18:48:06 +0100

    Add a new BAPSSetupV2.
    Essentially tried to replicate the old BAPSSetup with a new Installer Project, and actually make it install the registry items. It didn't seem to do that before.

--- BAPSServerService/ProjectInstaller.Designer.cs
+++ BAPSServerService/ProjectInstaller.Designer.cs
@@ -37,6 +37,7 @@ namespace BAPSServerService
             this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalService;
             this.serviceProcessInstaller1.Password = null;
             this.serviceProcessInstaller1.Username = null;
+            this.serviceProcessInstaller1.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceProcessInstaller1_AfterInstall);
             // 
             // serviceInstaller1
             // 
